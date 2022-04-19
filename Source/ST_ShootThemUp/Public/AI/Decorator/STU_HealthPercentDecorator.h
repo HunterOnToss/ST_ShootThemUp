cc79a158dc26b7aa71ef395d25cb6b2cba1c2582
StanHunter <Stasdolg@mail.ru>
2022-04-20 01:01:04 +0700

    Реализовал Декоратор - для поиска здоровья

--- Source/ST_ShootThemUp/Public/AI/Decorator/STU_HealthPercentDecorator.h
+++ Source/ST_ShootThemUp/Public/AI/Decorator/STU_HealthPercentDecorator.h
@@ -10,5 +10,14 @@ UCLASS()
 class ST_SHOOTTHEMUP_API USTU_HealthPercentDecorator : public UBTDecorator
 {
 	GENERATED_BODY()
-	
+
+public:
+    USTU_HealthPercentDecorator();
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AI")
+    float HealthPercent = 0.5f;
+    
+    virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+    
 };
