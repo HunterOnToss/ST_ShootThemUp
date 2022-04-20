f44813c9ca888c923b06fd18d46c1bcb26a6c40b
StanHunter <Stasdolg@mail.ru>
2022-04-20 18:29:42 +0700

    Определение свой чужой

--- Source/ST_ShootThemUp/Public/STUUtils.h
+++ Source/ST_ShootThemUp/Public/STUUtils.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "STUPlayerState.h"
 
 class STUUtils
 {
@@ -13,5 +14,15 @@ public:
             return Cast<T>(Component);
         }
         return nullptr;
+	}
+
+    bool static AreEnemies(const AController* Controller1, const AController* Controller2)
+	{
+	    if (!Controller1 || !Controller2 || Controller1==Controller2) return false;
+
+	    const auto PlayerState1 = Cast<ASTUPlayerState>(Controller1->PlayerState);
+	    const auto PlayerState2 = Cast<ASTUPlayerState>(Controller2->PlayerState);
+
+	    return PlayerState1 && PlayerState2 && PlayerState1->GetTeamID() != PlayerState2->GetTeamID();
 	}
 };
