7e5959e051d7da0c5c1d1fcd3e0006152d2adfb8
StanHunter <Stasdolg@mail.ru>
2022-04-21 04:07:02 +0700

    Подшаманил настройки

--- Source/ST_ShootThemUp/Public/UI/STUGameDataWidget.h
+++ Source/ST_ShootThemUp/Public/UI/STUGameDataWidget.h
@@ -15,9 +15,17 @@ class ST_SHOOTTHEMUP_API USTUGameDataWidget : public UUserWidget
 	GENERATED_BODY()
 
 public:
+
+    UFUNCTION(BlueprintCallable, Category="UI")
     int32 GetKillsNum() const;
+
+    UFUNCTION(BlueprintCallable, Category="UI")
     int32 GetCurrentRoundNum() const;
+
+    UFUNCTION(BlueprintCallable, Category="UI")
     int32 GetTotalRoundsNum() const;
+
+    UFUNCTION(BlueprintCallable, Category="UI")
     int32 GetRoundSecondsRemaining() const;
 
 private:
