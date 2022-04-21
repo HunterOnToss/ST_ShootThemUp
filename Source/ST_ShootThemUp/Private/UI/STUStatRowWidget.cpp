c38d9e9a1e8365792e1530062d9c8a7d30b8d692
StanHunter <Stasdolg@mail.ru>
2022-04-22 03:59:33 +0700

    Установка данных, для виджета Статистики

--- Source/ST_ShootThemUp/Private/UI/STUStatRowWidget.cpp
+++ Source/ST_ShootThemUp/Private/UI/STUStatRowWidget.cpp
@@ -2,4 +2,46 @@
 
 
 #include "UI/STUStatRowWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
 
+
+void USTUStatRowWidget::SetPlayerName(const FText& Text)
+{
+    if (PlayerNameTextBlock)
+    {
+        PlayerNameTextBlock->SetText(Text);
+    }
+}
+
+void USTUStatRowWidget::SetKills(const FText& Text)
+{
+    if (KillsTextBlock)
+    {
+        KillsTextBlock->SetText(Text);
+    }
+}
+
+void USTUStatRowWidget::SetDeaths(const FText& Text)
+{
+    if (DeathsTextBlock)
+    {
+        DeathsTextBlock->SetText(Text);
+    }
+}
+
+void USTUStatRowWidget::SetTeam(const FText& Text)
+{
+    if (TeamTextBlock)
+    {
+        TeamTextBlock->SetText(Text);
+    }
+}
+
+void USTUStatRowWidget::SetPlayerIndicatorVisibility(bool Visible)
+{
+    if (PlayerIndicatorImage)
+    {
+        PlayerIndicatorImage->SetVisibility(Visible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+    }
+}
--- Source/ST_ShootThemUp/Public/UI/STUStatRowWidget.h
+++ Source/ST_ShootThemUp/Public/UI/STUStatRowWidget.h
@@ -6,12 +6,37 @@
 #include "Blueprint/UserWidget.h"
 #include "STUStatRowWidget.generated.h"
 
-/**
- * 
- */
+class UImage;
+class UTextBlock;
+
+
 UCLASS()
 class ST_SHOOTTHEMUP_API USTUStatRowWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+
+    void SetPlayerName(const FText& Text);
+    void SetKills(const FText& Text);
+    void SetDeaths(const FText& Text);
+    void SetTeam(const FText& Text);
+    void SetPlayerIndicatorVisibility(bool Visible);
+    
+protected:
+
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* PlayerNameTextBlock;
+
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* KillsTextBlock;
+
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* DeathsTextBlock;
+
+    UPROPERTY(meta=(BindWidget))
+    UTextBlock* TeamTextBlock;
+
+    UPROPERTY(meta=(BindWidget))
+    UImage* PlayerIndicatorImage;    
 };
