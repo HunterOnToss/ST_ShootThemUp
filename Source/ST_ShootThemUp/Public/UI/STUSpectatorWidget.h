0ad7167549445d59165dfb25f558e5f8f985ec72
StanHunter <Stasdolg@mail.ru>
2022-04-21 13:40:44 +0700

    Виджет для отсчёта перерождения

---
+++ Source/ST_ShootThemUp/Public/UI/STUSpectatorWidget.h
@@ -0,0 +1,16 @@
+// Shoot Them Up Game. All Right Reserved 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STUSpectatorWidget.generated.h"
+
+
+UCLASS()
+class ST_SHOOTTHEMUP_API USTUSpectatorWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+    
+};
