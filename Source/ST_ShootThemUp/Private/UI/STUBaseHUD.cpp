d6cabc4a091d4608267bfcba961992b3ee52184b
StanHunter <Stasdolg@mail.ru>
2022-04-22 00:31:42 +0700

    Подрефакторил чуть-чуть класс

--- Source/ST_ShootThemUp/Private/UI/STUBaseHUD.cpp
+++ Source/ST_ShootThemUp/Private/UI/STUBaseHUD.cpp
@@ -4,19 +4,22 @@
 #include "UI/STUBaseHUD.h"
 #include "Engine/Canvas.h"
 #include "Blueprint/UserWidget.h"
+#include "STUGameModeBase.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSTUGameHUD, All, All);
 
 void ASTUBaseHUD::DrawHUD() 
 {
     Super::DrawHUD();
 
-    //DrawCrosshair();
+    //DrawCrossHair();
 }
 
 void ASTUBaseHUD::BeginPlay() 
 {
     Super::BeginPlay();
 
-    auto PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHudWidgetKlass);
+    const auto PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHudWidgetKlass);
     
     if (PlayerHUDWidget)
     {
@@ -24,15 +27,20 @@ void ASTUBaseHUD::BeginPlay()
     }
 }
 
-void ASTUBaseHUD::DrawCrosshair()
+void ASTUBaseHUD::DrawCrossHair()
 {
     const TInterval<float> Center(Canvas->SizeX * 0.5f, Canvas->SizeY * 0.5f);
     
-    const float HalfLineSize = 10.0f;
-    const float LineThickness = 2.0f;
+    constexpr float HalfLineSize = 10.0f;
+    constexpr float LineThickness = 2.0f;
     const FLinearColor LineColor = FLinearColor::Green;
 
     DrawLine(Center.Min - HalfLineSize, Center.Max, Center.Min + HalfLineSize, Center.Max, LineColor, LineThickness);
     DrawLine(Center.Min, Center.Max - HalfLineSize, Center.Min, Center.Max + HalfLineSize, LineColor, LineThickness);
+    
+}
+
+void ASTUBaseHUD::OnMatchStateChange(ESTUMatchState State)
+{
     
 }
