c3344b53d450170ebfe2437ea0fa9b05051e3c3c
StanHunter <Stasdolg@mail.ru>
2022-04-25 20:26:41 +0700

    Начал работу над Healthbar

---
+++ Source/ST_ShootThemUp/Public/UI/STUHealathBarWidget.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All Right Reserved 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STUHealathBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ST_SHOOTTHEMUP_API USTUHealathBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
