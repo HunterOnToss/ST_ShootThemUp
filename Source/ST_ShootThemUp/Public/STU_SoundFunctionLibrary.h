43f9f11fe8b3b146916cb1b3c58e6d95498739aa
StanHunter <Stasdolg@mail.ru>
2022-04-30 00:46:58 +0700

    Либа для помощи в управление звуком

---
+++ Source/ST_ShootThemUp/Public/STU_SoundFunctionLibrary.h
@@ -0,0 +1,15 @@
+// Shoot Them Up Game. All Right Reserved 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "STU_SoundFunctionLibrary.generated.h"
+
+
+UCLASS()
+class ST_SHOOTTHEMUP_API USTU_SoundFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
